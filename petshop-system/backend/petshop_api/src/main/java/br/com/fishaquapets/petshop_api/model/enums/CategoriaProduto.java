5f4d71bbcd0b5f3ef0481e47eef27b4ce0861853
Ryan de Souza Silva <ryan.souzasilvap1@gmail.com>
2026-03-24 10:41:26 -0300

    Enums utilizados em model
    Adicionadas categorias de produto e serviço

---
+++ petshop-system/backend/petshop_api/src/main/java/br/com/fishaquapets/petshop_api/model/enums/CategoriaProduto.java
@@ -0,0 +1,11 @@
+package br.com.fishaquapets.petshop_api.model.enums;
+
+public enum CategoriaProduto {
+    RACAO,
+    BRINQUEDO,
+    VIVEIRO,
+    ACESSORIOS,
+    SAUDE,
+    COMIDA
+    // Criar as demais futuramente
+}
--- petshop-system/backend/petshop_api/src/main/java/br/com/fishaquapets/petshop_api/model/enums/CategoriaServico.java
+++ petshop-system/backend/petshop_api/src/main/java/br/com/fishaquapets/petshop_api/model/enums/CategoriaServico.java
@@ -3,5 +3,8 @@ package br.com.fishaquapets.petshop_api.model.enums;
 public enum CategoriaServico {
     LIMPEZA,
     MANUTENCAO,
-    INSTALACAO
+    INSTALACAO,
+    DOMESTICACAO,
+    ANIMAL
+    // Criar as demais futuramente
 }
