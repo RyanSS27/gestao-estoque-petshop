a92f26c4f07eaa9c28edd19a57a92862f1014c7c
Ryan de Souza Silva <ryan.souzasilvap1@gmail.com>
2026-04-30 23:44:33 -0300

    Correção nos Enums

--- petshop-system/petshop-api/petshop-api/src/main/java/com/fishaquapets/petshop_api/model/enums/EstadoPagamento.java
+++ petshop-system/petshop-api/petshop-api/src/main/java/com/fishaquapets/petshop_api/model/enums/EstadoPagamento.java
@@ -1,12 +1,23 @@
 package com.fishaquapets.petshop_api.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum EstadoPagamento {
     PENDENTE(1),
     PAGAMENTO_PARCIAL(2),
     PAGA(3),
     ESTORNADA(4);
 
-    // ainda não terminei o enum, não precisa corrigir
+    private int code;
+
     EstadoPagamento(int i) {
+        this.code = i;
+    }
+
+    public static EstadoPagamento valueOff(int i) {
+        for(EstadoPagamento valor: EstadoPagamento.values()) {
+            if(valor.getCode() == i) return valor;
+        }
     }
 }
--- petshop-system/petshop-api/petshop-api/src/main/java/com/fishaquapets/petshop_api/model/enums/MetodoPagamento.java
+++ petshop-system/petshop-api/petshop-api/src/main/java/com/fishaquapets/petshop_api/model/enums/MetodoPagamento.java
@@ -1,8 +1,22 @@
 package com.fishaquapets.petshop_api.model.enums;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public enum MetodoPagamento {
-    DINHEIRO,
-    PIX,
-    CARTAO_DEBITO,
-    CARTAO_CREDITO
+    DINHEIRO(1),
+    PIX(2),
+    CARTAO_DEBITO(3),
+    CARTAO_CREDITO(4);
+
+    private int code;
+
+    MetodoPagamento(int code) { this.code = code; }
+
+    private static MetodoPagamento valueOf(int i) {
+        for(MetodoPagamento valor: MetodoPagamento.values()) {
+            if (valor.getCode() == i) return valor;
+        }
+    }
 }
