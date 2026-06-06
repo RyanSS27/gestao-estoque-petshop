f88a159a037981c3ef33bfaf236792c2078eb47b
Ryan de Souza Silva <ryan.souzasilvap1@gmail.com>
2026-06-06 18:12:30 -0300

    Mudanças nos enums de venda

--- petshop-system/petshop-api/petshop-api/src/main/java/com/fishaquapets/petshop_api/model/enums/PaymentMethod.java
+++ petshop-system/petshop-api/petshop-api/src/main/java/com/fishaquapets/petshop_api/model/enums/PaymentMethod.java
@@ -4,19 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum PaymentMethod {
-    DINHEIRO(1),
-    PIX(2),
-    CARTAO_DEBITO(3),
-    CARTAO_CREDITO(4);
-
-    private int code;
-
-    PaymentMethod(int code) { this.code = code; }
-
-    private static PaymentMethod valueOf(int i) {
-        for(PaymentMethod valor: PaymentMethod.values()) {
-            if (valor.getCode() == i) return valor;
-        }
-        throw new IllegalArgumentException("Invalid OrderStatus code");
-    }
+    DINHEIRO,
+    PIX,
+    CARTAO_DEBITO,
+    CARTAO_CREDITO;
 }
